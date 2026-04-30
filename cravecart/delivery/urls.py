19c766ebfa436ab2f5e834e055dcbcd9d5e2ba76
Jenn2004 <jenifer.j2004@gmail.com>
2026-04-30 15:57:44 +0530

    feat: implement signup and signin views

--- cravecart/delivery/urls.py
+++ cravecart/delivery/urls.py
@@ -3,5 +3,8 @@ from django.urls import include, path # pyright: ignore[reportMissingModuleSourc
 from . import views
 
 urlpatterns = [
-    path('', views.home)
+    path('', views.home, name="home"),
+    path("open_signup", views.open_signup, name="open_signup"),
+    path("open_signin", views.open_signin, name="open_signin"),
+    path("signup", views.signup, name="signup")
 ]
--- cravecart/delivery/views.py
+++ cravecart/delivery/views.py
@@ -1,5 +1,25 @@
+from django.http import HttpResponse   # pyright: ignore[reportMissingModuleSource] # ✔️ CORRECT
+
 from django.shortcuts import render # pyright: ignore[reportMissingModuleSource]
 
 # Create your views here.
+#home page
 def home(request):
     return render(request, "index.html")
+#sign up page
+def open_signup(request):
+    return render(request, "signUp.html")
+#sign in page
+def open_signin(request):
+    return render(request, "signIn.html")
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        email = request.POST.get('email')
+        phno = request.POST.get('phno')
+        address = request.POST.get('address')
+        return HttpResponse("Signup successfully!")
+    else:
+        return HttpResponse("Invalid credentials")
+        
